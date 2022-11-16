6d7c88b541bf39fe58f353f564d820ab286d90d2
Keemchard <keemchardtamio@gmail.com>
2022-11-16 14:34:10 +0800

    note delete option template

---
+++ src/notesComponents/NoteDeleteOptionForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NoteDeleteOptionForm = ({
+  permanentDelete,
+  cancelDelete,
+  noteId,
+  noteTitle,
+}: any) => {
+  return <div>NoteDeleteOptionForm</div>;
+};
+
+export default NoteDeleteOptionForm;
