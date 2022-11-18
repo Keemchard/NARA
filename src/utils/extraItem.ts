5990bb855ffa40739db452961a58cfebcf82aa62
Keemchard <keemchardtamio@gmail.com>
2022-11-18 15:17:42 +0800

    fix extra item

--- src/utils/extraItem.ts
+++ src/utils/extraItem.ts
@@ -5,14 +5,14 @@ const extraItems: extraModel[] = [
     id: Math.random(),
     title: "C19",
     short_description:
-      "It is a web app that allow the user to see number of infected, recovered, and etc cases of covid19 oncertain county",
+      "It is a web app that allows the user to view the number of Covid19 infected, recovered, and other cases in a specific county.",
     link: "https://c19data-kc.netlify.app/",
   },
   {
     id: Math.random(),
     title: "Pokedex",
     short_description:
-      "It is a web app that allow the user to search all pokemons and see their images along with their types and abilities",
+      "It is a web application that allows the user to search for all pokemons and view their images, as well as their types and abilities.",
     link: "https://pokedex-kc.netlify.app/",
   },
   {
